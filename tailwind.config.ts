e2e51be79e0032a180048811f2a616296c779512
Will <william.souza@raroprestador.com.br>
2025-02-08 22:03:00 -0300

    feat: add text-base style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        base: "var(--text-base)",
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
